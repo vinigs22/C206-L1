90053fa68091f9d62806d67af7ffe862d61f7601
Vinicius Gonçalves Simoni <102326974+vinigs22@users.noreply.github.com>
2024-02-29 20:06:38 -0300

    Add files via upload

---
+++ Aula1/Exercicio1.java
@@ -0,0 +1,7 @@
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        System.out.print("Minha primeira aula de POO com Java");
+    }
+
+}
---
+++ Aula1/Exercicio2.java
@@ -0,0 +1,9 @@
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Minha primeira aula de POO com Java");
+        System.out.println("Estamos usando o Netbeans!");
+    }
+
+}
